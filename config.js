dc90a542c5a57950859800eb79f18ece1c594d3d
Bill3612 <104715303+Bill3612@users.noreply.github.com>
2022-05-01 17:05:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "confess",
+    introTitle: 'Hiền không cao 😚',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnIntro: 'hế luuuu 😚😚😚',
+    title: 'T tạo ra trang web này để nói vs m điều này 👉👈',
+    desc: 'Cảm ơn m trong thời gian qua đã ở bên t ☺️☺️☺️ và đồng thời trong thời gian qua t cảm thấy rất thích m á ☺️ m có cảm thấy như vậy khum',
+    btnYes: 'Coá 😚',
+    btnNo: 'Mơ à 😒',
+    question: 'Và giờ là câu hỏi cuối nè :Đ m đồng ý làm người yêu t nhé:333',
+    btnReply: 'Gửi cho kải zũ ☺️',
+    reply: 'Đồng ý ❤️',
+    mess: 'Ỏ 😚😚😚',
+    messDesc: 'Thứ 4 này mình lại gặp nhau nhé h ib lại cho t nào :333',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
